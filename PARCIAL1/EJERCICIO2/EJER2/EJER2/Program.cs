f7354a10f462826e0aec6d0287dc2ddd37e481bf
YULIAN ALEXIS PAREJA ESTRADA <yulianpareja1128580@correo.itm.edu.co>
2024-09-13 22:53:18 -0500

    FINALIZACION DEL EJERCICIO 2

--- PARCIAL1/EJERCICIO2/EJER2/EJER2/Program.cs
+++ PARCIAL1/EJERCICIO2/EJER2/EJER2/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿public class Program
+{
+
+
+    static void Main(string[] args)
+    {
+        //VARIABLES
+        double A, B;
+        //INICIALIZAR
+        Console.WriteLine("INGRESE EL VALOR DE A:");
+        A=Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("INGRESE EL VALOR DE B:");
+        B = Convert.ToDouble(Console.ReadLine());
+        //OPERAR
+      
+            if (A == 0 && B == 0)
+            { Console.WriteLine("LA ECUACION TIENE INFINITAS SOLUCIONES.");}
+            else if (A == 0)
+            {Console.WriteLine("LA ECUACION NO TIENE SOLUCION."); }
+            else
+            {double X = -B / A;
+                Console.WriteLine($"LA SOLUCION DE LA ECUACION ES : {X}");
+            }
+        }
+    }
+
+
+
+
+
+
+
