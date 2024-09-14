77db8c5987cbefd808c7caed03224061026bb79b
YULIAN ALEXIS PAREJA ESTRADA <yulianpareja1128580@correo.itm.edu.co>
2024-09-13 21:32:50 -0500

    DEFINIR VARIABLES

---
+++ PARCIAL1/EJERCICIO1/EJERCICIO1/EJERCICIO1/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Program
+{ 
+ 
+
+ static void Main(string[] args)
+    {
+        //CONSTANTES
+        const Double SUELDO_BASE = 150000, COMISION = 0.10, BENEFICIO = 100000;
+        //VARIABLES: SUELDO,VENTAS Y COMISIONES
+        Double VENTA1,VENTA2,VENTA3,COMISION1,COMISION2,COMISION3,COMISIONTOTAL,SUELDOTOTAL,VENTATOTAL;
+        //INICIALIZAR LAS VARIABLES
+        Console.WriteLine("INGRESE EL VALOR DE SU PRIMER VENTA");
+        VENTA1=Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("INGRESE EL VALOR DE SU SEGUNDA VENTA");
+        VENTA2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("INGRESE EL VALOR DE SU TERCER VENTA");
+        VENTA3 = Convert.ToDouble(Console.ReadLine());
+        //PROCESOS
+        //1-¿Cuánto dinero obtendrá por concepto de comisiones por las tres ventas que realiza en el mes
+        COMISIONTOTAL = ()
+
+
+    }
+}
+
