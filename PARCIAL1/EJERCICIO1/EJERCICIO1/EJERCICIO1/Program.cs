ebb8ea72e07e1f2b9a3d27d4abaabccb40c1a17e
YULIAN ALEXIS PAREJA ESTRADA <yulianpareja1128580@correo.itm.edu.co>
2024-09-13 22:14:14 -0500

    EJERCICIO FINALIZADO

--- PARCIAL1/EJERCICIO1/EJERCICIO1/EJERCICIO1/Program.cs
+++ PARCIAL1/EJERCICIO1/EJERCICIO1/EJERCICIO1/Program.cs
@@ -7,7 +7,7 @@
         //CONSTANTES
         const Double SUELDO_BASE = 150000, COMISION = 0.10, BENEFICIO = 100000;
         //VARIABLES: SUELDO,VENTAS Y COMISIONES
-        Double VENTA1,VENTA2,VENTA3,COMISION1,COMISION2,COMISION3,COMISIONTOTAL,SUELDOTOTAL,VENTATOTAL;
+        Double VENTA1,VENTA2,VENTA3,COMISION1,COMISION2,COMISION3,COMISIONTOTAL,SUELDOTOTAL,VENTATOTAL,PROMEDIO;
         //INICIALIZAR LAS VARIABLES
         Console.WriteLine("INGRESE EL VALOR DE SU PRIMER VENTA");
         VENTA1=Convert.ToDouble(Console.ReadLine());
@@ -17,7 +17,22 @@
         VENTA3 = Convert.ToDouble(Console.ReadLine());
         //PROCESOS
         //1-¿Cuánto dinero obtendrá por concepto de comisiones por las tres ventas que realiza en el mes
-        COMISIONTOTAL = ()
+        COMISIONTOTAL = ((COMISION) * (VENTA1 + VENTA2 + VENTA3));
+        Console.WriteLine("EL DINERO QUE OBTENDRA EN COMISIONES POR LAS 3 VENTAS ES DE:"+ COMISIONTOTAL);
+        //¿Cuál es el total que recibirá en el mes tomando en cuenta su sueldo base y comisiones? 
+        SUELDOTOTAL = (COMISIONTOTAL + SUELDO_BASE);
+        //¿Cuál fue la venta que le generó la mayor comisión? 
+        if (VENTA1 > VENTA2 && VENTA1 > VENTA3)
+        { Console.WriteLine("LA VENTA1 FUE LA QUE GENERO MAYOR COMISION:"); }
+        else if (VENTA2 > VENTA1 && VENTA2 > VENTA3)
+        { Console.WriteLine("LA VENTA2 FUE LA QUE GENERO MAYOR COMISION:"); }
+        else
+        { Console.WriteLine("LA VENTA3 FUE LA QUE GENERO MAYOR COMISION:"); }
+        //¿Cuál fue el promedio de las comisiones que recibió por cada venta? 
+        PROMEDIO = (COMISIONTOTAL / 3);
+        Console.WriteLine("EL PROMEDIO QUE RECIBIO EN COMISIONES POR CADA VENTA ES DE:" + PROMEDIO);
+
+
 
 
     }
